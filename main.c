f2684d14881f48ef6ed6d1700d39912e05e4a79a
jacobin <harly.pe@gmail.com>
2024-11-22 15:56:20 +0800

    const char* basename(const char* argv0)

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <fcntl.h>     /* for _O_TEXT and _O_BINARY */
-
+#include <io.h>      /* for _setmode() */
 #include "md5.h"
 
 void print_hash(uint8_t* p) {
@@ -12,33 +12,39 @@ void print_hash(uint8_t* p) {
     printf("\n");
 }
 
-int main(int argc, char* argv[]) {
-    const char* p1 = argv[0] - 1, * p2 = argv[0];
-    for (char* p = argv[0]; 1; p++)
+const char* basename(const char* argv0)
+{
+    const char* p1 = argv0 - 1, * p2 = argv0;
+    for (const char* p = argv0; 1; p++)
     {
         const char c = *p;
         if (c == '\0' || c == '\\' || c == '/' || c == ':')
         {
             p1 = p2;
             p2 = p;
-        }
-        if (c == '\0')
-        {
-            break;
+            if (c == '\0')
+            {
+                break;
+            }
         }
     }
+    return p1 + 1;
+}
 
+int main(int argc, char* argv[]) {
     uint8_t result[16];
     if (argc > 1) {
         const char* g = argv[1];
-        if (strcmp(g, "/?") || strcmp(g, "-h") || strcmp(g, "--help"))
+        if (!strcmp(g, "/?") || !strcmp(g, "-h") || !strcmp(g, "--help"))
         {
-            fprintf(stderr, "%s [targetString]\r\n"
+            const char* bname = basename(argv[0]);
+            fprintf(stderr,
+                "%s {[targetString1] [targetString2] ... [targetStringN]}\r\n"
                 "\tExample:\r\n"
-                "\t\tmd5 \"The quick brown fox jumps over the lazy dog\"\r\n"
-                "\t\tmd5 < \"c:\\windows\\explorer.exe\"\r\n"
-                "\t\ttype \"c:\\Windows\\System32\\drivers\\gmreadme.txt\" | md5"
-                , p1 + 1);
+                "\t\t%s \"The quick brown fox jumps over the lazy dog\"\r\n"
+                "\t\t%s < \"c:\\windows\\explorer.exe\"\r\n"
+                "\t\ttype \"c:\\Windows\\System32\\drivers\\gmreadme.txt\" | %s"
+                , bname, bname, bname, bname);
             exit(1);
         }
         for (int i = 1; i < argc; ++i) {
@@ -47,9 +53,13 @@ int main(int argc, char* argv[]) {
         }
     }
     else {
-        // FILE* fp = fopen("d:\\a.bat", "r");
-        // FILE* fp = fopen("d:\\a.bat", "rb");
-        _setmode(_fileno(stdin), O_BINARY);
+        // FILE* fp = fopen("d:\\target.file", "r");
+        // FILE* fp = fopen("d:\\target.file", "rb");
+        if (-1 == _setmode(_fileno(stdin), O_BINARY))
+        {
+            fprintf(stderr, "\"_setmode(_fileno(stdin), O_BINARY)\" failed.");
+            abort(); // exit(3);
+        }
         md5File(stdin, result);
         print_hash(result);
     }
